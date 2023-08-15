0c5422387dcf36ed353133c7b35eecc03bf1f155
Arun <113204762+rajput-arun110@users.noreply.github.com>
2023-08-15 11:30:09 +0530

    JS file

---
+++ script.js
@@ -0,0 +1,29 @@
+// import React,{Component} from 'React';
+// import './styles.css';
+// import Header from './components/Header/Header'
+// import {library} from '@fortawesome/fontawesome-svg-core'
+// import {fas} from '@fortawesome/free-solid-svg-icons'
+// import CrouselView from './components/Crousel/Crousel';
+// import DealCardviewPort from './components/DealCardViewPort';
+// import BrandInFocusViewPort from './components/BrandInFocusViewPort/BrandViewPort'
+// library add (fas)
+
+
+// class App extends Component{
+//     render(){
+//     return(
+//         <div className = "Appcontainer-fluid">
+//             <Header/>
+//             <CrouselView/>
+//             <DealCardviewPort/>
+//             <h2> BRANDS IN FOCUS</h2>
+//             <p>Show some brand</p>
+//             <BrandInFocusViewPort/>
+//         <div/>
+//         );
+//     }
+        
+// }   export default App;
+            
+
+    
